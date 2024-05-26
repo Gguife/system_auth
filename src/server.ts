cd76ab96375275224e84d6e9842212f4f44ed980
Gguife <gguife747@gmail.com>
2024-05-26 11:52:53 -0300

    feat: Added routes and logs for database connection

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
-import * as express from 'express';
-import { Router, Request, Response } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import routes from './routes/router';
+import database from '../src/database/config/conn';
+
+dotenv.config();
 
 const app = express();
+app.use(routes);
+app.use(cors);
 
-const route = Router();
-
-app.use('/', (req: Request, res: Response) => {
-  res.json({message: 'Hello world'})
-});
-
-
-app.use(route);
+database.sync().then(() => {
+  console.log(`Database is connected: ${process.env.DB_NAME}`);
+}).catch(error =>{
+  console.error('Unable to connect to the database:', error)
+})
 
 app.listen(8080, () => 'Server running on port 8080');
