a9c7b58614e728a68fede5a136417665aab878ce
Gguife <gguife747@gmail.com>
2024-05-30 18:01:51 -0300

    fix:  Name host for connection with DB

--- src/database/config/conn.ts
+++ src/database/config/conn.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import { DB_NAME, DB_HOST, DB_PASSWORD, DB_USER } from '../../interfaces/configs';
 
 let sequelizeConnection: Sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
+  host: 'mysql',
   dialect: 'mysql',
   port: 3306, 
 });
