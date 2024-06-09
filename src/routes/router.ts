d812db2c056a5567b2d3460ee7a29804ef263d2f
Gguife <gguife747@gmail.com>
2024-06-09 20:34:47 -0300

    feat: Created better name for url

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,10 +4,10 @@ import user from "../controller/user";
 const route = Router();
 
 route.get('/users', user.findAllUsers);
-route.get('/users/:id', user.findOneUser);
-route.post('/users', user.addUser);
-route.put('/users/:id', user.updateUser);
-route.delete('/users/:id', user.removeUser);
+route.get('/account/:id', user.findOneUser);
+route.post('/account/create', user.addUser);
+route.put('/account/update/:id', user.updateUser);
+route.delete('/account/:id', user.removeUser);
 
 
 export default route;
