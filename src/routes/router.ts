5b16d8ec6b991f7186565fec028721a15ebc55fb
Gguife <gguife747@gmail.com>
2024-08-09 18:47:52 -0300

    fix: Change endpoints

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import user from "../controller/user";
+import { authenticateUser } from "../middlewares/authMiddleware";
 
 const route = Router();
 
-route.get('/users', user.findAllUsers);
-route.get('/account/:id', user.findOneUser);
-route.post('/account/create', user.addUser);
-route.put('/account/update/:id', user.updateUser);
-route.delete('/account/remove/:id', user.removeUser);
-route.post('/login', user.loginUser);
+route.get('/user/:id', user.getUser);
+route.post('/account/create', user.createUser);
+route.post('/login', authenticateUser, user.loginUser);
 
 export default route;
