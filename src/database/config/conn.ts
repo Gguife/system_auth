351742b3888e7ccfb3b87906e4f45359f5dce3b5
Gguife <gguife747@gmail.com>
2024-08-10 20:25:36 -0300

    Fix: Romeve unusable data

--- src/database/config/conn.ts
+++ src/database/config/conn.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import { DB_NAME, DB_HOST, DB_PASSWORD, DB_USER } from '../../interfaces/configs';
+import { DB_NAME, DB_PASSWORD, DB_USER } from '../../interfaces/configs';
 
 let sequelizeConnection: Sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: 'mysql',
