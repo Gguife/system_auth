5fd931a6e9705e240b6c03612323f3cddb029b96
Gguife <gguife747@gmail.com>
2024-08-19 18:53:33 -0300

    perf: Best practices for more readable code

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -4,32 +4,21 @@ import UserModel from '../database/models/userModel';
 import passwordService from '../service/userServices/passwordService';
 
 
-export const authtenticateUser = async (req: Request, res: Response, next: NextFunction) => {
-  try{
-    const { email, password } = req.body;
-    const ipAddr = req.socket.remoteAddress || 'unknown-ip';
-    const user = await UserModel.findOne({ where: { email } });
+export const authUserMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  const ipAddr = req.socket.remoteAddress || 'unknown-ip';
+  const user = await UserModel.findOne({ where: { email } });
 
-    if (!email || !password) {
-      return res.status(400).json({ error: 'Email and password are required.' });
-    }
+  if(!user) return res.status(404).json({error: "User not found"});
 
-    if (await LoginAttemptService.isLockedOut(ipAddr)) {
-      return res.status(429).json({ error: 'Too many login attempts. Please try again later.' });
-    }
+  if (await LoginAttemptService.isLockedOut(ipAddr)) return res.status(429).json({ error: 'Too many login attempts. Please try again later.' });
 
-    if (!user) throw new Error("User not found.");
-    
-    const isMatch = await passwordService.comparePassword(password, user.password);
-    if (!isMatch) {
-      await LoginAttemptService.recordFailedAttempt(ipAddr);
-      res.status(401).json({error: 'Invalid credentials!'});
-    }
-
-    req.user = user; 
-    next();
-  }catch(error){
-      console.error("Unexpected error during authentication:", error);
-      res.status(500).json({ error: 'An unexpected error occurred during authentication.' });
+  const isMatch = await passwordService.comparePassword(password, user.password);
+  if (!isMatch) {
+    await LoginAttemptService.recordFailedAttempt(ipAddr);
+    return res.status(401).json({ error: 'Invalid Credentials.' });
   }
+  
+  req.user = user;
+  next();
 }
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,13 +1,13 @@
 import { Router, Response, Request } from "express";
 import user from "../controller/user";
-import { authtenticateUser } from "../middlewares/authMiddleware";
+import { authUserMiddleware } from "../middlewares/authMiddleware";
 import { authtenticateToken } from "../middlewares/jwtMiddleware";
 
 const router = Router();
 
 router.get('/user/:id', user.getUser);
 router.post('/account/create', user.createUser);
-router.post('/login', authtenticateUser, user.loginUser);
+router.post('/login', authUserMiddleware, user.loginUser);
 
 router.get('/private', authtenticateToken, (req: Request, res: Response) => {
   res.json(req.user);
